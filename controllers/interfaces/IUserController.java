f7fd09b8053e19cc8fbe75a0c52fcfcbc5d69039
Bekdos <241396@astanait.edu.kz>
2025-01-25 09:19:23 +0500

    added more in User

--- controllers/interfaces/IUserController.java
+++ controllers/interfaces/IUserController.java
@@ -1,4 +1,7 @@
 package controllers.interfaces;
 
 public interface IUserController {
+    String createUser(String name, String surname, boolean gender);
+    String getUser(int id);
+    String getAllUsers();
 }

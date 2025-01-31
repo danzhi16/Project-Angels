7205a236882752bab011413f034c7735b9097e06
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-01-31 10:59:19 +0500

    A LOT OF NEW CHANGES

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import repositories.interfaces.IUserRepository;
 
 public class Main {
     public static void main(String[] args) {
-            IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "290607", "AngelsOOP");
+            IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "12345678", "AngelsOOP");
 
         IUserRepository repo = new UserRepository(db);
         IUserController controller = new UserController(repo);
